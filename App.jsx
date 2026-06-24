049a2ffb5ba566d6403c02f8e20c45e8e206080a
kashj05 <kashishj201@gmail.com>
2026-06-24 22:40:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import HeroNavbar from "./components/HeroNavbar";
+import Overview from "./components/Overview";
+import WhyParticipate from "./components/WhyParticipate";
+import RecognitionProcess from "./components/RecognitionProcess";
+import ContactUs from "./components/ContactUs";
+
+function App() {
+  return (
+    <> 
+    <HeroNavbar />
+    <Overview />
+    <WhyParticipate />
+    <RecognitionProcess />
+    <ContactUs />
+    </>
+  );
+}
+
+export default App;
